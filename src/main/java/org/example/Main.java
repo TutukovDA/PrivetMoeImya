8b75162bcb3df3e3860795fa9333db9fbf5a47df
TutukovDA <126241028+TutukovDA@users.noreply.github.com>
2023-03-06 23:23:16 +0400

    Initial commit  Напишите программу на Java, которая напечатает «Hello» на экране, а затем напечатайте свое имя в отдельной строке.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Привет");
+        System.out.println("Дмитрий Андреевич");
+        System.out.print("Thank You");
+        System.out.println("Ruslan Damirovich");
+    }
+}
